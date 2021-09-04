967265f679b44ee504dc828aee0ba70572a40cf6
cristianceban <cristian.ceban@lynxsolutions.eu>
2021-09-04 18:33:43 +0300

    eslint fixed

--- src/components/Color/Color.tsx
+++ src/components/Color/Color.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { Dimensions, StyleSheet } from "react-native";
 import Animated, {
-	Extrapolate,
-	interpolate,
-	runOnJS,
-	useAnimatedGestureHandler,
-	useAnimatedStyle,
+  Extrapolate,
+  interpolate,
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
 } from "react-native-reanimated";
 import {
-	TapGestureHandler,
-	TapGestureHandlerGestureEvent,
+  TapGestureHandler,
+  TapGestureHandlerGestureEvent,
 } from "react-native-gesture-handler";
 
 const { width } = Dimensions.get("window");
@@ -19,68 +19,68 @@ export const COLOR_WIDTH = width / 3;
 const RADIUS = 45;
 
 const styles = StyleSheet.create({
-	container: {
-		width: COLOR_WIDTH,
-		alignItems: "center",
-	},
-	gradient: {
-		borderRadius: RADIUS,
-		width: RADIUS * 2,
-		height: RADIUS * 2,
-		borderWidth: 6,
-		borderColor: "white",
-	},
+  container: {
+    width: COLOR_WIDTH,
+    alignItems: "center",
+  },
+  gradient: {
+    borderRadius: RADIUS,
+    width: RADIUS * 2,
+    height: RADIUS * 2,
+    borderWidth: 6,
+    borderColor: "white",
+  },
 });
 
 interface ColorProps {
-	color: {
-		start: string;
-		end: string;
-	};
-	index: number;
-	translateX: Animated.SharedValue<number>;
-	onPress: (position: { x: number; y: number }) => void;
+  color: {
+    start: string;
+    end: string;
+  };
+  index: number;
+  translateX: Animated.SharedValue<number>;
+  onPress: (position: { x: number; y: number }) => void;
 }
 
 const Color = ({ color, translateX, index, onPress }: ColorProps) => {
-	const inputRange = [
-		-COLOR_WIDTH * (index + 1),
-		-COLOR_WIDTH * index,
-		-COLOR_WIDTH * (index - 1),
-	];
-	const wrapper = (args) => onPress({ ...args });
-	const onGestureEvent =
-		useAnimatedGestureHandler<TapGestureHandlerGestureEvent>({
-			onActive: ({ absoluteX: x, absoluteY: y }) => {
-				runOnJS(wrapper)({ x, y });
-			},
-		});
-	const style = useAnimatedStyle(() => {
-		const angle = interpolate(
-			translateX.value,
-			inputRange,
-			[0, Math.PI / 2, Math.PI],
-			Extrapolate.CLAMP
-		);
-		const translateY = 100 * Math.cos(angle);
-		const scale = 0.8 + 0.2 * Math.sin(angle);
+  const inputRange = [
+    -COLOR_WIDTH * (index + 1),
+    -COLOR_WIDTH * index,
+    -COLOR_WIDTH * (index - 1),
+  ];
+  const wrapper = (args) => onPress({ ...args });
+  const onGestureEvent =
+    useAnimatedGestureHandler<TapGestureHandlerGestureEvent>({
+      onActive: ({ absoluteX: x, absoluteY: y }) => {
+        runOnJS(wrapper)({ x, y });
+      },
+    });
+  const style = useAnimatedStyle(() => {
+    const angle = interpolate(
+      translateX.value,
+      inputRange,
+      [0, Math.PI / 2, Math.PI],
+      Extrapolate.CLAMP
+    );
+    const translateY = 100 * Math.cos(angle);
+    const scale = 0.8 + 0.2 * Math.sin(angle);
 
-		return {
-			transform: [{ translateX: translateX.value }, { translateY }, { scale }],
-		};
-	});
-	return (
-		<Animated.View style={[styles.container, style]}>
-			<TapGestureHandler onGestureEvent={onGestureEvent}>
-				<Animated.View>
-					<LinearGradient
-						colors={[color.start, color.end]}
-						style={styles.gradient}
-					/>
-				</Animated.View>
-			</TapGestureHandler>
-		</Animated.View>
-	);
+    return {
+      transform: [{ translateX: translateX.value }, { translateY }, { scale }],
+    };
+  });
+  return (
+    <Animated.View style={[styles.container, style]}>
+      <TapGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View>
+          <LinearGradient
+            colors={[color.start, color.end]}
+            style={styles.gradient}
+          />
+        </Animated.View>
+      </TapGestureHandler>
+    </Animated.View>
+  );
 };
 
 export default Color;
